fcbe7ebd2468725fb564c876d968e3dc04ace954
Sven Willenbuecher <sven.willenbuecher@kuehne-nagel.com>
2023-11-22 15:52:46 +0100

    continue

--- cmd/hit/flags.go
+++ cmd/hit/flags.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"net/url"
 	"os"
 	"strings"
 )
@@ -17,12 +18,12 @@ func (f *flags) parse() error {
 	flag.StringVar(&f.url, "url", "", "HTTP server `URL` to make requests (required)")
 	flag.IntVar(&f.n, "n", f.n, "Number of requests to make")
 	flag.IntVar(&f.c, "c", f.c, "Concurrency level")
+	flag.Parse()
 	if err := f.validate(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		flag.Usage()
 		return err
 	}
-	flag.Parse()
 	return nil
 }
 
@@ -31,8 +32,16 @@ func (f *flags) validate() error {
 	if f.c > f.n {
 		return fmt.Errorf("-c=%d: should be less than or equal to -n=%d", f.c, f.n)
 	}
-	if strings.TrimSpace(f.url) == "" {
-		return errors.New("-url: required")
+	if err := validateURL(f.url); err != nil {
+		return fmt.Errorf("invalid value %q for flag -url: %w", f.url, err)
 	}
 	return nil
+}
+
+func validateURL(s string) error {
+	if strings.TrimSpace(s) == "" {
+		return errors.New("required")
+	}
+	_, err := url.Parse(s)
+	return err
 }
